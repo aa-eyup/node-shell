6ab788a3605a436c6ea4fea38fd9901b46389cc8
Alan Watson <alanowatson@gmail.com>
2021-01-15 16:32:02 -0500

    curl completed

--- curl.js
+++ curl.js
@@ -1,9 +1,11 @@
 const request = require('request');
 
-module.exports = process.stdout.reqst;
+module.exports = curling;
 
-let reqst = request(siteName, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+function curling(siteName) {
+  request(siteName, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+}
